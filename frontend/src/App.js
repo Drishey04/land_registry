681b16aebc23ab182db30e1fb55007605f26a068
Drishey Singh <drisheysingh@gmail.com>
2023-03-25 11:24:09 +0530

    first commit

---
+++ frontend/src/App.js
@@ -0,0 +1,28 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { createTheme } from "@mui/material/styles";
+import { themeSettings } from "./theme";
+import LandingPage from "scenes/landingpage/index.jsx";
+import UserDashboard from "scenes/userdashboard/index.jsx";
+
+function App() {
+
+  const theme = createTheme(themeSettings());
+  
+  return (
+    <div className="App">
+      <BrowserRouter>
+      <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Routes>
+            <Route path="/" element={<LandingPage />} />
+            <Route path="/user" element={<UserDashboard />} />
+ 
+          </Routes>
+      </ThemeProvider>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
